632d83fd8255793eafe17789414d05c8fd4bb735
avcjeewantha <avcjeewantha@gmail.com>
2019-03-02 22:38:53 +0530

    BackButton and GetHighScore functions

--- Bring - Go/Assets/MainMenu.cs
+++ Bring - Go/Assets/MainMenu.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
 
 public class MainMenu : MonoBehaviour
 {
+    private string connectionString;
+
+    public void highScore()
+    {
+        connectionString = "URI=file:" + Application.dataPath + "/HighScoreDB.sqlite";
+        getHighScoreFromDB();
+    }
 
     public void PlayGame()
     {
@@ -15,6 +25,58 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void getHighScoreFromDB()
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT MAX(score) FROM HighScore";
+                dbCmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader.GetValue(0));                       
+                    }
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
     }
     
 }
