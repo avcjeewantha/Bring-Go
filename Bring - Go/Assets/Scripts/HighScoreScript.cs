599f481f42d3f87cd9a181a69d18a8eb950f75e6
avcjeewantha <avcjeewantha@gmail.com>
2019-03-21 15:15:04 +0530

    code restructured

--- Bring - Go/Assets/Scripts/HighScoreScript.cs
+++ Bring - Go/Assets/Scripts/HighScoreScript.cs
@@ -2,25 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mono.Data.Sqlite;
+using System;
+using System.Data;
 
 public class HighScoreScript : MonoBehaviour
 {
-    public GameObject score;
+    public GameObject score; 
+    private static string connectionString;
+    public static String _score;
+    private static HighScore highscore;
 
-    //public void setScore(string score)
-    //{
-      //  this.score.GetComponent<Text>().text = score;
-        //Debug.Log(score);
-    //}
-    //void Start()
-    //{
-
-    //}
-
-    // Update is called once per frame
-    void Update()   //Update the high Score (text)
+    void Update()   //Update the high Score (text)    // Update is called once per frame
     {
-        this.score.GetComponent<Text>().text = MainMenu.score;
+        this.score.GetComponent<Text>().text = _score;
+    }
+
+    public static void showHighScore()               //executes the update method in HighScoreScript 
+    {
+        connectionString = "URI=file:" + Application.dataPath + "/HighScoreDB.sqlite";
+        highscore = null;
+
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT MAX(score) FROM HighScore";
+                dbCmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        highscore = new HighScore(reader.GetInt32(0));   //Create a new High Score Object
+                        //Debug.Log(this.highscore.score.ToString());
+                    }
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
+        _score = highscore.score.ToString();
+    }
+
+    public void insertScore(int newScore)   //Insert new scores to database
+    {
+        connectionString = "URI=file:" + Application.dataPath + "/HighScoreDB.sqlite";
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("INSERT INTO HighScore(score) VALUES(\"{0}\"", newScore);
+                dbCmd.CommandText = sqlQuery;
+                dbCmd.ExecuteScalar();
+                dbConnection.Close();
+            }
+        }
+    }
+
+    private void deleteScoreFromDB(int oldScore)   // can delete specific score from database
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("DELETE FROM HighScore WHERE score = \"{0}\"", oldScore);
+                dbCmd.CommandText = sqlQuery;
+                dbCmd.ExecuteScalar();
+                dbConnection.Close();
+            }
+        }
     }
 
 }
--- Bring - Go/Assets/Scripts/MainMenu.cs
+++ Bring - Go/Assets/Scripts/MainMenu.cs
@@ -2,111 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
-using System.Data;
-using Mono.Data.Sqlite;
 
 public class MainMenu : MonoBehaviour
 {
-    private string connectionString;
-    private HighScore highscore;
     public GameObject scorePreFab;
-    public static String score;
-
-    public void highScore()         //Get the high score
-    {
-        connectionString = "URI=file:" + Application.dataPath + "/HighScoreDB.sqlite";
-        getHighScoreFromDB();
-    }
-
-    public void insertScore(int newScore)   //Insert new scores to database
-    {
-        connectionString = "URI=file:" + Application.dataPath + "/HighScoreDB.sqlite";
-        insertScoreToDB(newScore);
-    }
-
-    public void showHighScore()               //executes the update method in HighScoreScript 
-    {
-        highScore();
-        score = this.highscore.score.ToString();
-        //Debug.Log(this.highscore.score.ToString());
-        //GameObject tmpObjec = Instantiate(scorePreFab);
-        //tmpObjec.transform.SetParent(, false);
-        //Debug.Log("chhsd");
-       // HighScore tmpScore = this.highscore;
-        //Debug.Log(tmpScore.score.ToString());
-       // tmpObjec.GetComponent<HighScoreScript>().setScore(tmpScore.score.ToString());
-    }
-
 
     public void PlayGame()    //Start to play the game
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }   
+    }
 
+    public void showHighScore()               //executes the update method in HighScoreScript 
+    {
+        HighScoreScript.showHighScore();
+    }
+    
     public void QuitGame()      //Exit from the application
     {
         //Debug.Log("QUIT!");
         Application.Quit();
-    }
-
-    private void getHighScoreFromDB()       //Get the high score from database
-    {
-        this.highscore = null;
-
-        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
-        {
-            dbConnection.Open();
-
-            using (IDbCommand dbCmd = dbConnection.CreateCommand())
-            {
-                string sqlQuery = "SELECT MAX(score) FROM HighScore";
-                dbCmd.CommandText = sqlQuery;
-
-                using (IDataReader reader = dbCmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        this.highscore = new HighScore(reader.GetInt32(0));   //Create a new High Score Object
-                        //Debug.Log(this.highscore.score.ToString());
-                    }
-                    dbConnection.Close();
-                    reader.Close();
-                }
-            }
-        }
-    }
-
-    private void insertScoreToDB(int newScore)    // Inser Scores to the Database
-    {
-        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
-        {
-            dbConnection.Open();
-
-            using (IDbCommand dbCmd = dbConnection.CreateCommand())
-            {
-                string sqlQuery = String.Format("INSERT INTO HighScore(score) VALUES(\"{0}\"",newScore);  
-                dbCmd.CommandText = sqlQuery;
-                dbCmd.ExecuteScalar();
-                dbConnection.Close();
-            }
-        }
-    }
-
-    private void deleteScoreFromDB(int oldScore)   // can delete specific score from database
-    {
-        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
-        {
-            dbConnection.Open();
-
-            using (IDbCommand dbCmd = dbConnection.CreateCommand())
-            {
-                string sqlQuery = String.Format("DELETE FROM HighScore WHERE score = \"{0}\"", oldScore );
-                dbCmd.CommandText = sqlQuery;
-                dbCmd.ExecuteScalar();
-                dbConnection.Close();
-            }
-        }
     }
     
 }
