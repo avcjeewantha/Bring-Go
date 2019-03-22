bfa482c37d099d8d413ebea3f7c24de2fcafb3ac
avcjeewantha <avcjeewantha@gmail.com>
2019-03-23 00:05:39 +0530

    pause menu

---
+++ Bring - Go/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+
+    public void resume(){
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void pause(){
+        Time.timeScale = 0f;
+        gameIsPaused = true; 
+    }
+
+    public void getMainMenu(){
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public void quit(){
+        Application.Quit();
+    }
+
+}
