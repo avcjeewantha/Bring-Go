3b47ed49592f7e45ad2505f9ba50a8a4426f19d0
avcjeewantha <avcjeewantha@gmail.com>
2019-03-22 17:15:12 +0530

    object moves within the device screen

---
+++ Bring - Go/Assets/Scripts/playerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // float horizontal = 50f;
+    // float vertical = 50f;
+    // float y = 50f;
+
+    // void Start()     // Start is called before the first frame update
+    // {
+        
+    // }
+
+    // void Update()     // Update is called once per frame
+    // {
+    //     GetComponent<Rigidbody>().velocity = new Vector3(horizontal, vertical, y);
+    // }
+
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    void Start(){
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+}
